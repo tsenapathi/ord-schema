3873b3c36fb8a1d6c985a0db6f7ed283e42ee7b9
Nathaniel Kim <nkim8@mit.edu>
2020-04-13 14:25:42 -0700

    Update form examples, make CSS work

--- webform/docker/editor/views.py
+++ webform/docker/editor/views.py
@@ -7,3 +7,9 @@ def index(request):
     template = loader.get_template('editor/webform.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def small(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('editor/small.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
