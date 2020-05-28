ecf15b5e726a07ba67a85d840bddc16ee0d38617
Nathaniel Kim <nkim8@mit.edu>
2020-05-27 18:17:49 -0700

    Use validation in webform (and install prereqs)

--- webform/docker/editor/views.py
+++ webform/docker/editor/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template import loader
 from django.views.decorators.http import require_http_methods
-from editor import reaction_pb2
+from editor import reaction_pb2, validations
 
 def clone(request):
     template = loader.get_template('editor/clone.html')
